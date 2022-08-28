41dc2933e8c2dcfaeeb5c56bf678127486c69d1a
yaofeil <yaofei.liu@student.unimelb.edu.au>
2022-08-28 13:47:09 +1000

    reduce redundant code
    reduce redundant code

--- src/I.java
+++ src/I.java
@@ -16,25 +16,25 @@ class I extends Actor
     super();
     this.tetris = tetris;
     // rotId 0
-    r[0][0] = new Location(new Location(-1, 0));
-    r[1][0] = new Location(new Location(0, 0));
-    r[2][0] = new Location(new Location(1, 0));
-    r[3][0] = new Location(new Location(2, 0));
+    r[0][0] = new Location(-1, 0);
+    r[1][0] = new Location(0, 0);
+    r[2][0] = new Location(1, 0);
+    r[3][0] = new Location(2, 0);
     // rotId 1
-    r[0][1] = new Location(new Location(0, -1));
-    r[1][1] = new Location(new Location(0, 0));
-    r[2][1] = new Location(new Location(0, 1));
-    r[3][1] = new Location(new Location(0, 2));
+    r[0][1] = new Location(0, -1);
+    r[1][1] = new Location(0, 0);
+    r[2][1] = new Location(0, 1);
+    r[3][1] = new Location(0, 2);
     // rotId 2
-    r[0][2] = new Location(new Location(-1, 0));
-    r[1][2] = new Location(new Location(0, 0));
-    r[2][2] = new Location(new Location(1, 0));
-    r[3][2] = new Location(new Location(2, 0));
+    r[0][2] = new Location(-1, 0);
+    r[1][2] = new Location(0, 0);
+    r[2][2] = new Location(1, 0);
+    r[3][2] = new Location(2, 0);
     // rotId 3
-    r[0][3] = new Location(new Location(0, -1));
-    r[1][3] = new Location(new Location(0, 0));
-    r[2][3] = new Location(new Location(0, 1));
-    r[3][3] = new Location(new Location(0, 2));
+    r[0][3] = new Location(0, -1);
+    r[1][3] = new Location(0, 0);
+    r[2][3] = new Location(0, 1);
+    r[3][3] = new Location(0, 2);
 
     for (int i = 0; i < r.length; i++)
       blocks.add(new TetroBlock(blockId, r[i]));
